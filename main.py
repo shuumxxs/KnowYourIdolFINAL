ecdf3bd0abea6fd62c2423c27d82b6c052b87b0d
shuumxxs <paulene.magarde@gmail.com>
2026-03-06 00:28:30 +0800

    a LOT of menu work

--- main.py
+++ main.py
@@ -1,8 +1,93 @@
+import json
 
 try:
     filename = "questions.json"
     with open(filename, 'r') as file:
         data = json.load(file)
+
+    def menu():
+        print("[0] START!")
+        print("[1] INSTRUCTIONS!")
+        print("[2] CREDITS!")
+        print("[3] EXIT!")
+
+
+    def c0():
+        global exo_pts
+        global njz_pts
+        global twc_pts
+        global plv_pts
+        global lsf_pts
+        global tvq_pts
+        print()
+        print("CHOOSE A GROUP!")
+        print()
+        print()
+        print("[1] EXO")
+        print()
+        print("[2] NEWJEANS")
+        print()
+        print("[3] TWICE")
+        print()
+        print("[4] PLAVE")
+        print()
+        print("[5] LE SSERAFIM")
+        print()
+        print("[6] TVXQ!")
+        print()
+        print("[7] WIP")
+        print()
+        print("[8] WIP")
+        print()
+        print("[9] WIP")
+        print()
+
+    exo_pts = 0
+    njz_pts = 0
+    twc_pts = 0
+
+
+    plv_pts = 0
+    lsf_pts = 0
+    tvq_pts = 0
+
+    print("✧･ﾟ: *✧･ﾟ:*:･ﾟ✧*:･ﾟ✧" * 5)
+    print("\n\nWelcome to Know your Idol! \n(Press Enter to continue.)")
+    silly = input(print("\n\n : 3 "))
+    print("✧･ﾟ: *✧･ﾟ:*:･ﾟ✧*:･ﾟ✧" * 5)
+    print("\n\n ")
+
+
+    menu()
+
+    ans = int(input("\nYour choice? : "))
+
+    while True:
+        if ans == 0:
+            c0()
+        elif ans == 1:
+            c1()
+        elif ans == 2:
+            c2()
+        elif ans == 3:
+            print("are you sure? y/n : ")
+            while True:
+                leave = str(input("your choice:"))
+                if leave == "y":
+                    print("Awh man!! okay..")
+                    break
+                elif leave == "n":
+                    print("oh, nevermind!")
+                else:
+                    print("invalid input :,(")
+                break
+
+        else:
+            print("invalid input.")
+            print("")
+        start_menu()
+        ans = int(input("Your choice? "))
+
 
 
 
