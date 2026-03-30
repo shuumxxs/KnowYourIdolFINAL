55f6c59df0ee466cb12ffa04116c4f8237f754dc
shuumxxs <paulene.magarde@gmail.com>
2026-03-30 21:10:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import json
 
 try:
-    filename = "questions.json"
+    filename = "q.json"
     with open(filename, 'r') as file:
         data = json.load(file)
 
     def menu():
         print("[0] START!")
         print("[1] INSTRUCTIONS!")
-        print("[2] SCORE!")
-        print("[3] EXIT!")
+        print("[2] EXIT!")
+
+
+    exo_pts = 0
+    njz_pts = 0
+    twc_pts = 0
+
+    plv_pts = 0
+    lsf_pts = 0
+    tvq_pts = 0
+
+    enh_pts = 0
+    skz_pts = 0
+    mtx_pts = 0
 
 
     def c0():
@@ -45,7 +57,7 @@ try:
         print("[9] MONSTA X")
         print()
 
-        ans = int(input("\nYour choice? : "))
+        ans = int(input("\nYour choice? (strings will end the program!) : "))
     
         while True:
             if ans == 1:
@@ -57,12 +69,21 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            exo_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                exo_pts =+ 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {exo_pts}/5!")
+                break
+
                             
             elif ans == 2:
                 
@@ -73,13 +94,23 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            njz_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
-                
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                njz_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {njz_pts}/5!")
+                break
+
+
+
             elif ans == 3:
                 
                 for q in data:
@@ -89,13 +120,22 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            twc_pts =+ 1
-                            print("/nCorrect!")
-                        else:
-                            print("/nIncorrect!")
-                
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                twc_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {twc_pts}/5!")
+                break
+
+
             elif ans == 4:
                 
                 for q in data:
@@ -105,12 +145,22 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            plv_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
+                        ans = input("Your choice? ").upper()
+
+                        while True:
+                            if ans == q["correct"]:
+                                plv_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {plv_pts}/5!")
+                break
+
 
 
             elif ans == 5:
@@ -122,12 +172,20 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            lsf_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                lsf_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {lsf_pts}/5!")
+                break
 
             elif ans == 6:
                 
@@ -138,12 +196,21 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            tvq_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                tvq_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {tvq_pts}/5!")
+                break
+
             elif ans == 7:
                 
                 for q in data:
@@ -153,12 +220,21 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            enh_pts =+ 1
-                            print("/nCorrect!")
-                        else:
-                            print("/nIncorrect!")
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                enh_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {enh_pts}/5!")
+                break
+
             elif ans == 8:
                 
                 for q in data:
@@ -168,13 +244,22 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            skz_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
-            else:
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                skz_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            elif ans != str:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {skz_pts}/5!")
+                break
+
+            elif ans == 9:
                 for q in data:
                     if q["group"] == "mtx":
                         print(q["q"])
@@ -182,79 +267,69 @@ try:
                         print(f"[B] {q["B"]}")
                         print(f"[C] {q["C"]}")
                         print(f"[D] {q["D"]}")
-                        ans = input("Your choice?").upper()
-                        if ans == q["correct"]:
-                            mtx_pts =+ 1
-                            print("/nCorrect!") 
-                        else:
-                            print("/nIncorrect!")
-            start()
+                        ans = input("Your choice? ").upper()
+                        while True:
+                            if ans == q["correct"]:
+                                mtx_pts = + 1
+                                print("\nCorrect!")
+                                break
+                            elif ans != q["correct"]:
+                                print("\nIncorrect!")
+                                break
+                            else:
+                                print("\nNot a valid input!")
+                                ans = input("Your choice? ").upper()
+                print(f"\n                Score: {mtx_pts}/5!")
+                break
+
+            elif ans != int:
+                print("\nNot a valid input!")
+                ans = input("Your choice? ").upper()
+
+            else:
+                print("\nNot a valid input!")
+                ans = input("Your choice? ").upper()
+
 
     def c1():
         print("words")
 
-    def c2():
-        global exo_pts
-        global njz_pts
-        global twc_pts
-        global plv_pts
-        global lsf_pts
-        global tvq_pts
-        global enh_pts
-        global skz_pts
-        global mtx_pts
-                
 
-    exo_pts = 0
-    njz_pts = 0
-    twc_pts = 0
-
-
-    plv_pts = 0
-    lsf_pts = 0
-    tvq_pts = 0
-
-    enh_pts = 0
-    skz_pts = 0
-    mtx_pts = 0
-
-    print("✧･ﾟ: *✧･ﾟ:*:･ﾟ✧*:･ﾟ✧" * 5)
+    print("*^-+" * 5)
     print("\n\nWelcome to Know your Idol! \n(Press Enter to continue.)")
-    silly = input(print("\n\n : 3 "))
-    print("✧･ﾟ: *✧･ﾟ:*:･ﾟ✧*:･ﾟ✧" * 5)
-    print("\n\n ")
+    silly = input(": 3 ")
+    print(silly)
+    print("*^-+" * 5)
 
 
-    def start():
+
+    while True:
         menu()
-    
-        ans = int(input("\nYour choice? : "))
-    
-        while True:
-            if ans == 0:
-                c0()
-            elif ans == 1:
-                c1()
-            elif ans == 2:
-                c2()
-            elif ans == 3:
-                print("are you sure? y/n : ")
-                while True:
-                    leave = str(input("your choice:"))
-                    if leave == "y":
-                        print("Awh man!! okay..")
-                        break
-                    elif leave == "n":
-                        print("oh, nevermind!")
-                    else:
-                        print("invalid input :,(")
+
+        ans = int(input("\nYour choice? (strings will end the program!): "))
+
+        if ans == 0:
+            c0()
+        elif ans == 1:
+            c1()
+        elif ans == 2:
+            print("are you sure? y/n")
+            leave = str(input("your choice : ")).lower()
+            while True:
+
+                if leave == "y":
+                    print("Awh man!! okay..")
                     break
-    
-            else:
-                print("invalid input.")
-                print("")
-            menu()
-            ans = int(input("Your choice? "))
+                elif leave == "n":
+                    print("oh, nevermind!")
+                else:
+                    print("invalid input :,(")
+                    leave = input("your choice:")
+
+
+        else:
+            print("\nNot a valid input!")
+            ans = int(input("Your choice? : "))
 
 
 
